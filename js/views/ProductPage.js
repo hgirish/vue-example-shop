515d41baf0ee35e00b9741d2f18bb8346b49db59
hgirish <girish@post.com>
2018-12-13 21:28:00 -0800

    product thumbnails

--- js/views/ProductPage.js
+++ js/views/ProductPage.js
@@ -3,13 +3,20 @@ const ProductPage = {
   template: `
   <div>
   <div v-if="product">
-  <div class="images" v-if="product.images.length">
+  <div class="images" v-if="image">
+  <div class="main">
+  <img :src="image.source"
+  :alt="image.alt || product.title" width="300" />
+  </div>
+  <div class="thumbnails" v-if="product.images.length > 1">
   <template v-for="img in product.images">
 <img
 :src="img.source"
 :alt="img.alt || product.title"
-width="100">
+width="100"
+@click="updateImage(img)" />
   </template>
+  </div>
   </div>
   <h1>{{product.title}}</h1>
   <div class="meta">
@@ -28,20 +35,32 @@ width="100">
   },
   data() {
     return {
-      productNotFound: false
+      productNotFound: false,
+      image: false,
     }
   },
   computed: {
     product() {
       let product;
+
       if (Object.keys(this.$store.state.products).length) {
         product = this.$store.state.products[this.$route.params.slug];
+        if (product) {
+          this.productNotFound = false;
+          this.image = (product.images.length) ? product.images[0] : false;
+        }
+
         if (!product) {
           this.productNotFound = true;
         }
         console.log(product);
       }
       return product;
+    }
+  },
+  methods: {
+    updateImage(img) {
+      this.image = img;
     }
   }
 };
